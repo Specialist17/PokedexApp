d6e145479ffa2730f17818d4abae78d546574e39
Specialist17 <Specialist@Fernandos-MacBook-Pro-2.local>
2015-11-08 01:30:42 -0400

    added custom class for collection view and connected the imageView and label from the pokeCell class to the Stroyboard

---
+++ Pokedex by Specialist/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  Pokedex by Specialist
+//
+//  Created by Fernando on 11/8/15.
+//  Copyright © 2015 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+
+}
