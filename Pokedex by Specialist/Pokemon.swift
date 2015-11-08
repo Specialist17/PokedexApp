3c792a7e592d3ad7646ab5dbe40bad5e32a21584
Specialist17 <Specialist@Fernandos-MacBook-Pro-2.local>
2015-11-08 00:59:09 -0400

    added pokemon class and started with the interface

---
+++ Pokedex by Specialist/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex by Specialist
+//
+//  Created by Fernando on 11/8/15.
+//  Copyright © 2015 Fernando. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
