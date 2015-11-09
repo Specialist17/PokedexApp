41a17ef96f875981ffba23e9b23f212a7d092126
Specialist17 <Specialist@Fernandos-MacBook-Pro-2.local>
2015-11-08 20:37:05 -0400

    added functionality to the search bar and dismiss the keyboard when pressing Done or touching the screen

--- Pokedex by Specialist/ViewController.swift
+++ Pokedex by Specialist/ViewController.swift
@@ -7,20 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
         collection.delegate = self
         collection.dataSource = self
+        searchBar.delegate = self
         
+        //Keyboard gets the "done" key
+        searchBar.returnKeyType = UIReturnKeyType.Done
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        initAudio()
         parsePokemonCSV()
+    }
+    
+    func initAudio(){
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do{
+            
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: path))
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV(){
@@ -52,7 +81,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell{
             
-            let poke = pokemon[indexPath.row]
+            let poke: Pokemon!
+            
+            if inSearchMode{
+                poke = filteredPokemon[indexPath.row]
+            }else{
+                poke = pokemon[indexPath.row]
+            }
+            
+            
             cell.configureCell(poke)
             
             return cell
@@ -69,7 +106,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode{
+            return filteredPokemon.count
+        }
+        
+        return pokemon.count
+        
+        
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -78,6 +121,59 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
+    }
+    
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        
+        if musicPlayer.playing{
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        }else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            
+            //close the keyboard
+            view.endEditing(true)
+            collection.reloadData()
+        }else{
+            inSearchMode = true
+            // searchText.lowercaseString... searchText is provided by the function as a parameter so you can use it
+            // to make everything lowercase
+            let lower = searchText.lowercaseString
+            
+            
+            //$0 means it grabs an element out of the array and it gives it a name of 0
+            // $0 is the same as "var somePokemon = pokemon[23]"
+            //"filteredpokemon" gets the filtered array
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(lower) != nil })
+            
+            collection.reloadData()
+        }
+        
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    //closes the keyboard when touching on main view
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        self.collection.endEditing(true)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
 }
